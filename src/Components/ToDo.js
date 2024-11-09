d5386c771965d488c76865adccb3d2ac099b5a62
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-09 07:44:32 +0545

    changes

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-
 const ToDo = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
@@ -17,25 +16,43 @@ const ToDo = () => {
     setTasks(updatedTasks);
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTask();
+    }
+  };
+
   return (
-    <div className="todo-container">
-      <h2 className="fade-in">To-Do List</h2>
-      <div className="input-container fade-in">
+    <div className="todo-container animate-scale-in">
+      <h2 className="title animate-bounce-in">To-Do List</h2>
+      <div className="input-container animate-slide-down">
         <input
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
+          onKeyPress={handleKeyPress}
           placeholder="Add a new task"
+          className="animate-fade-in"
         />
-        <button onClick={handleAddTask} className="slide-in-right">
+        <button 
+          onClick={handleAddTask} 
+          className="add-button animate-pulse"
+        >
           Add Task
         </button>
       </div>
-      <ul className="fade-in">
+      <ul className="task-list">
         {tasks.map((task, index) => (
-          <li key={index} className="task slide-in-left">
-            {task}
-            <button onClick={() => handleDeleteTask(index)} className="slide-in-right">
+          <li 
+            key={index} 
+            className="task-item animate-slide-in"
+            style={{ animationDelay: `${index * 0.1}s` }}
+          >
+            <span className="task-text">{task}</span>
+            <button 
+              onClick={() => handleDeleteTask(index)} 
+              className="delete-button animate-shake-hover"
+            >
               Delete
             </button>
           </li>
